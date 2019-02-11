9089e6e6feebe7e65f3511ad023bff0d71897fd0
tako_desktop <takotako@gmail.com>
2019-02-11 14:52:03 +0100

    made the infernce work

--- lib/core/inference.py
+++ lib/core/inference.py
@@ -12,17 +12,18 @@ import math
 
 import numpy as np
 
-from utils.transforms import transform_preds
+from ..utils.transforms import transform_preds
 
 
 def get_max_preds(batch_heatmaps):
-    '''
+    """
     get predictions from score maps
     heatmaps: numpy.ndarray([batch_size, num_joints, height, width])
-    '''
-    assert isinstance(batch_heatmaps, np.ndarray), \
-        'batch_heatmaps should be numpy.ndarray'
-    assert batch_heatmaps.ndim == 4, 'batch_images should be 4-ndim'
+    """
+    assert isinstance(
+        batch_heatmaps, np.ndarray
+    ), "batch_heatmaps should be numpy.ndarray"
+    assert batch_heatmaps.ndim == 4, "batch_images should be 4-ndim"
 
     batch_size = batch_heatmaps.shape[0]
     num_joints = batch_heatmaps.shape[1]
@@ -59,16 +60,21 @@ def get_final_preds(config, batch_heatmaps, center, scale):
                 hm = batch_heatmaps[n][p]
                 px = int(math.floor(coords[n][p][0] + 0.5))
                 py = int(math.floor(coords[n][p][1] + 0.5))
-                if 1 < px < heatmap_width-1 and 1 < py < heatmap_height-1:
-                    diff = np.array([hm[py][px+1] - hm[py][px-1],
-                                     hm[py+1][px]-hm[py-1][px]])
-                    coords[n][p] += np.sign(diff) * .25
+                if 1 < px < heatmap_width - 1 and 1 < py < heatmap_height - 1:
+                    diff = np.array(
+                        [
+                            hm[py][px + 1] - hm[py][px - 1],
+                            hm[py + 1][px] - hm[py - 1][px],
+                        ]
+                    )
+                    coords[n][p] += np.sign(diff) * 0.25
 
     preds = coords.copy()
 
     # Transform back
     for i in range(coords.shape[0]):
-        preds[i] = transform_preds(coords[i], center[i], scale[i],
-                                   [heatmap_width, heatmap_height])
+        preds[i] = transform_preds(
+            coords[i], center[i], scale[i], [heatmap_width, heatmap_height]
+        )
 
     return preds, maxvals
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import cv2
+import numpy as np
+from pathlib import Path
+import torch
+import torchvision.transforms as transforms
+
 from lib import models
+from lib.core import inference
 
 # from lib.dataset import coco
 
@@ -7,7 +14,20 @@ def main():
     model = models.get_fully_pretrained_pose_net()
     model.eval()
     print(model)
-    # coco()
+
+    image_file = Path("data/coco/val2017/000000581781.jpg")
+    normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+    )
+    transform = transforms.Compose([transforms.ToTensor(), normalize])
+    # cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION
+    data_numpy = cv2.imread(
+        str(image_file), cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION
+    )
+
+    input = transform(data_numpy)
+    output = model(input[None, :, :, :]).detach().numpy()
+    print(inference.get_max_preds(output))
 
 
 if __name__ == "__main__":
